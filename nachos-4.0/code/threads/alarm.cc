f711e384aa4e065382a69d24a131684f6d6049a8
root <root@OSproject.myguest.virtualbox.org>
2023-11-23 10:43:39 +0800

    complete sleep

--- nachos-4.0/code/threads/alarm.cc
+++ nachos-4.0/code/threads/alarm.cc
@@ -46,13 +46,13 @@ Alarm::Alarm(bool doRandom)
 //	interrupts.  In this case, we can safely halt.
 //----------------------------------------------------------------------
 
-void 
-Alarm::CallBack() 
+void Alarm::CallBack() 
 {
     Interrupt *interrupt = kernel->interrupt;
     MachineStatus status = interrupt->getStatus();
-    
-    if (status == IdleMode) {	// is it time to quit?
+    bool woken = _bedroom.MorningCall();
+ 
+    if (status == IdleMode && !woken && _bedroom.IsEmpty()) {	// is it time to quit?
         if (!interrupt->AnyFutureInterrupts()) {
 	    timer->Disable();	// turn off the timer
 	}
@@ -61,3 +61,37 @@ Alarm::CallBack()
     }
 }
 
+void Alarm::WaitUntil(int x) {
+    IntStatus oldLevel = kernel->interrupt->SetLevel(IntOff);
+    Thread* t = kernel->currentThread;
+    //cout << "Alarm::WaitUntil go sleep" << endl;
+    _bedroom.PutToBed(t, x);
+    kernel->interrupt->SetLevel(oldLevel);
+}
+
+bool Bedroom::IsEmpty() {
+    return _beds.size() == 0;
+}
+
+void Bedroom::PutToBed(Thread*t, int x) {
+    ASSERT(kernel->interrupt->getLevel() == IntOff);
+    _beds.push_back(Bed(t, _current_interrupt + x));
+    t->Sleep(false);
+}
+
+bool Bedroom::MorningCall() {
+    bool woken = false;
+    _current_interrupt ++;
+    for(std::list<Bed>::iterator it = _beds.begin(); 
+        it != _beds.end(); ) {
+        if(_current_interrupt >= it->when) {
+            woken = true;
+            // cout << "Bedroom::MorningCall Thread woken" << endl;
+            kernel->scheduler->ReadyToRun(it->sleeper);
+            it = _beds.erase(it);
+        } else {
+            it++;
+        }
+    }
+    return woken;
+}
